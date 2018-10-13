3964ea898071d11b4157cbe0ec7d6354c9c5a533
Anthony <DeGariless@Gmail.com>
2018-10-13 07:06:43 -0700

    Update OC Server Spawn.lua

--- OC Server Spawn.lua
+++ OC Server Spawn.lua
@@ -1,70 +1,70 @@
---[[
-Spawn program made by DeGariless
-]]--
+AUTHORS_NOTE = [[Spawn program made by DeGariless]]
 
-require("term").clear()
-print("Initializing Spawn Program")
+require("term").clear() local args = {...}
 
+
+
+--######################
+--## GLOBAL VARIABLES ##
+--######################
+print("Initializing Global Variables")
+
+local TIMEZONEDB_API_KEY = "YOUR_TIMEZONEDB_API_KEY_GOES_HERE"
+local VERSION = "1.1.1"
+local PAGETIME = 12   --Default: 12
+local TABWIDTH = 20   --Default: 20
+
+--Resolution
+local w = 87  --Default: 87
+local h = 25  --Default: 25
+
+if TIMEZONEDB == "YOUR_TIMEZONEDB_API_KEY_GOES_HERE" then 
+  print("You needs a TimezoneDB API key")
+  print("Press any key to continue...")
+  event.pull("key_down")
+  error()
+end
+
+--###############
+--## LIBRARIES ##
+--###############
 print("Adding Custom Libraries")
+
 local component = require("component")
 local computer = require("computer")
 local inet = require("internet")
 local term = require("term")
 
+
+
+--################
+--## COMPONENTS ##
+--################
 print("Adding Components")
+
+assert(component.isAvailable("debug"), "No debug card installed")
+local db = component.debug
 local fs = component.filesystem
 local gpu = component.gpu
-if not component.isAvailable("onlinedetector") then
-  error("Could not find an Online Detector attached to the computer")
-end
-local od = component.onlinedetector
 
-local file = io.open("/lib/json.lua", "r")
+
+
+local file = io.open("/usr/lib/json4lua.lua", "r")
 if file == nil then
-  file = io.open("/lib/json.lua", "w")
-  print("Downloading JSON library")
-  local result, response = pcall(inet.request, "http://regex.info/code/JSON.lua")
-  if result then
-    print("Success")
-    for chunk in response do
-      file:write(chunk)
-    end
+  file = io.open("/usr/lib/json4lua.lua", "w")
+  print("Downloading JSON4Lua library")
+  local result, response = pcall(inet.request, "https://raw.githubusercontent.com/craigmj/json4lua/master/json/json.lua")
+  assert(result, "Could not download JSON4Lua library")
+  print("Success")
+  for chunk in response do
+    file:write(chunk)
   end
   file:close()
-  print("file saved to /lib/json.lua")
+  print("file saved to /usr/lib/json4lua.lua")
 end
 file = nil
-
-print("Loading JSON library")
-local JSON = loadfile("/lib/json.lua")()
-print("\n " .. JSON.AUTHOR_NOTE .. [[
-
- - Simple JSON encoding and decoding in pure Lua.
- -
- - Copyright 2010-2016 Jeffery Friedl
- - http://regex.info/blog
- - Latest version: regex.info.blog.lua.json
- -
- - This code is released under a Creative Commons CC-BY "Attribution" License:
- - http://creativecommons.org/licenses/by/3.0/deed.en_US
-]])
-os.sleep(5)
-
---######################
---## GLOBAL VARIABLES ##
---######################
-print("Setting global variables.")
-
-VERSION = "1.1.0"
-local args = {...}
-
-local PAGETIME = 15  -- Default is 15
-local TABWIDTH = 20
-
---Resolution
-local w = 87 -- Default: 87
-local h = 25 -- Default: 25
-
+print("Loading JSON4Lua library")
+local json = require("json4lua")
 
 
 --################
@@ -72,49 +72,49 @@ local h = 25 -- Default: 25
 --################
 print("Loading Files.")
 
-file = io.open("Rules.txt", "r")
+file = io.open("/Rules.txt", "r")
 if file == nil then
   print("Rules.txt not found. Creating default file.")
-  file = io.open("Rules.txt", "w")
+  file = io.open("/Rules.txt", "w")
   file:write("Add server rules in Rules.txt")
   file:close()
-  file = io.open("Rules.txt", "r")
+  file = io.open("/Rules.txt", "r")
 end
 local rules = file:read("*all")
 file:close()
 file = nil
 
-file = io.open("PlayerData.json", "r")
+file = io.open("/PlayerData.json", "r")
 if file == nil then
   print("PlayerData.json not found. Creating default file.")
-  file = io.open("PlayerData.json", "w")
+  file = io.open("/PlayerData.json", "w")
   file:write('{"owner":[],"staff":[],"players":[]}')
   file:close()
-  file = io.open("PlayerData.json", "r")
+  file = io.open("/PlayerData.json", "r")
 end
-local playerData = JSON:decode( file:read("*all") )
+local playerData = json.decode( file:read("*all") )
 file:close()
 file = nil
 
-file = io.open("Announcements.txt", "r")
+file = io.open("/Announcements.txt", "r")
 if file == nil then
   print("Announcements.txt not found. Creating default file.")
-  file = io.open("Announcements.txt", "w")
-  file:write("  There are no server announcementes at this time.")
+  file = io.open("/Announcements.txt", "w")
+  file:write("  There are no server announcements at this time.")
   file:close()
-  file = io.open("Announcements.txt", "r")
+  file = io.open("/Announcements.txt", "r")
 end
 local announcements = file:read("*all")
 file:close()
 file = nil
 
-file = io.open("FAQ.txt", "r")
+file = io.open("/FAQ.txt", "r")
 if file == nil then
   print("FAQ.txt not found. Creating default file.")
-  file = io.open("FAQ.txt", "w")
-  file:write("  Q: I can haz free diamonds?\n  A: Nope")
+  file = io.open("/FAQ.txt", "w")
+  file:write("  Q: How can I protect my base?\n  A: Edit this answer in /FAQ.txt")
   file:close()
-  file = io.open("FAQ.txt", "r")
+  file = io.open("/FAQ.txt", "r")
 end
 local faq = file:read("*all")
 file:close()
@@ -135,6 +135,7 @@ local theme = {
   text = 0xCC9933,
   textHigh = 0xFFCC99,
   online = 0x44FF44,
+  never = 0xFF8888,
   err = 0xFF0000  
 }
 
@@ -173,42 +174,31 @@ end
 
 local function time()  
   local req = nil
-  local data = nil
-  req = inet.request("http://www.timeapi.org/utc/now?format=%25Y%25j%25H%25M")
-  if not pcall(function () data = req() end) then
-    req:close()
-    return nil
+  local data = ""
+  req = inet.request("http://api.timezonedb.com/v2.1/get-time-zone?key=" .. TIMEZONEDB_API_KEY .. "&format=json&by=zone&zone=America/Los_Angeles")
+  for line in req do
+    data = data .. line
   end
-  if #data ~= 11 then
-    return nil
-  end
-  return data
+  return json.decode(data).timestamp
 end
 
 
-local function timeDif(pTime, curTime)
-  pTime = tostring(pTime)
-  local cYear = curTime:sub(1,4)
-  local cDay = curTime:sub(5,7)
-  local cHour = curTime:sub(8,9)
-  local cMin = curTime:sub(10,11)
-  local pYear = pTime:sub(1,4)
-  local pDay = pTime:sub(5,7)
-  local pHour = pTime:sub(8,9)
-  local pMin = pTime:sub(10,11)
-  local dYear = cYear - pYear
-  local dDay = cDay - pDay
-  local dHour = cHour - pHour
-  local dMin = cMin - pMin
-  local dif = math.floor(((dYear * 365 + dDay) * 24 + dHour) * 60 + dMin)  --dif is in minutes
-  if dif < 2 then
+local function timeHuman(dif)
+  --dif is in seconds
+  if dif < 5 then
     return "a moment ago"
   elseif dif < 60 then
-    return(dif.." minutes ago")
+    return(dif .. " seconds ago")
   end
-  dif = math.floor(dif/6+0.5)/10  --dif is in hours
+  dif = math.floor(dif/6+0.5)/10  -- convert dif to minutes
+  if dif <= 1 then
+    return "a minute ago"
+  elseif dif < 60 then
+    return(dif .. " minutes ago")
+  end 
+  dif = math.floor(dif/6+0.5)/10  -- convert dif to hours
   if dif == 1 then
-    return "1 hour ago"
+    return "an hour ago"
   elseif dif < 24 then
     return(dif.." hours ago")
   end
@@ -255,7 +245,7 @@ end
 
 
 local function updatePlayerData(curTime)
-  for _, onlinePlayer in ipairs(od.getPlayerList()) do
+  for _, onlinePlayer in ipairs(db.getPlayers()) do
     local found = false
     for _, v in pairs(playerData.owner) do
       if onlinePlayer == v.name then
@@ -289,15 +279,19 @@ local function updatePlayerData(curTime)
   end
   orderPlayerData()  
   local file = io.open("PlayerData.json", "w")
-  file:write(JSON:encode_pretty(playerData))
+  file:write(json.encode(playerData))
   file:close()
 end
 
 local function lastseen(player, curTime)
-  if od.isPlayerOnline(player.name) then
-    return "Online"
+  local status = db.getPlayer(player.name).getGameType()
+  if status == "spectator" or status == nil then
+    if player.lastseen == 0 then
+      return "Never"
+    end
+    return timeHuman(curTime - player.lastseen)
   else
-    return timeDif(player.lastseen, curTime)
+    return "Online"
   end
 end
 
@@ -342,7 +336,7 @@ end
 
 
 local function lastseenPage(curTime)
-  print("  Owner\n  "..string.rep("-", 60))
+  print("  Owner "..string.rep("_", 93))
   gpu.setForeground(theme.textHigh)
   term.write("  Name"..string.rep(" ", TABWIDTH - 4).."Last seen\n")
   gpu.setForeground(theme.text)
@@ -352,6 +346,8 @@ local function lastseenPage(curTime)
     local status = lastseen(v, curTime)
     if status == "Online" then
       gpu.setForeground(theme.online)
+    elseif status == "Never" then
+      gpu.setForeground(theme.never)
     end
     term.write(status)
     gpu.setForeground(theme.text)
@@ -362,7 +358,31 @@ local function lastseenPage(curTime)
     end
   end
   term.write("\n\n")
-  print("  Players\n  "..string.rep("-", 60))
+  
+  print("  Staff "..string.rep("_", 93))
+  gpu.setForeground(theme.textHigh)
+  term.write("  Name"..string.rep(" ", TABWIDTH - 4).."Last seen\n")
+  gpu.setForeground(theme.text)
+  for k,v in ipairs(playerData.staff) do
+    term.write("  "..v.name)
+    term.write(string.rep(".", TABWIDTH - #v.name))
+    local status = lastseen(v, curTime)
+    if status == "Online" then
+      gpu.setForeground(theme.online)
+    elseif status == "Never" then
+      gpu.setForeground(theme.never)
+    end
+    term.write(status)
+    gpu.setForeground(theme.text)
+    if k%2 == 0 then
+      print()
+    else
+      term.write(string.rep(" ", TABWIDTH - #status - 2))
+    end
+  end
+  term.write("\n\n")
+
+  print("  Players "..string.rep("_", 91))
   gpu.setForeground(theme.textHigh)
   term.write("  Name"..string.rep(" ", TABWIDTH-4).."Last seen\n")
   gpu.setForeground(theme.text)
@@ -372,6 +392,8 @@ local function lastseenPage(curTime)
     local status = lastseen(v, curTime)
     if status == "Online" then
       gpu.setForeground(theme.online)
+    elseif status == "Never" then
+      gpu.setForeground(theme.never)
     end
     term.write(status)
     gpu.setForeground(theme.text)
@@ -398,7 +420,7 @@ end
 --###################
 --## MAIN FUNCTION ##
 --###################
-print("Starting Program in 5 seconds!")
+print("Starting Program")
 
 local function main()
   gpu.setResolution(w, h)
@@ -433,18 +455,15 @@ end
 --## MAIN ##
 --##########
 
-local err = nil
-while err == "too long without yeilding" or err == nil do
-  pcall(function () os.sleep(5) end)
-  _, err = pcall(main)
-end
+local _, traceback, _ = xpcall(main, debug.traceback)
+
 
 
 --#################
 --## ERROR CATCH ##
 --#################
 
-gpu.setBackground(0x001100)
+gpu.setBackground(0x000000)
 gpu.setForeground(0xFFFFFF)
 gpu.fill(1, 1, w, h, " ")
 term.setCursor(1, 1)
@@ -455,7 +474,8 @@ print("  http://github.com/DeGariless/OC-Server-Spawn/")
 print("  make sure to include this error message...")
 print("  -----------")
 gpu.setForeground(theme.err)
-print("  " .. err)
+print(traceback)
 gpu.setForeground(0xFFFFFF)
 print()
-while true do os.sleep(60) end
+print("  Press any key to continue...")
+event.pull("key_down")
